628b6120ec9191b68ccb8938214db44a569a939f
DushyantPal1203 <dushyant.pal@hashstudioz.com>
2026-06-03 11:11:55 +0530

    Social account added in footer

--- integrations/hubspot.php
+++ integrations/hubspot.php
@@ -404,7 +404,7 @@ include ROOT_PATH . '/components/navbar.php';
                         Support
                     </a>
 
-                    <a href="<?php echo BASE_URL; ?>/404.php" class="btn btn-secondary">
+                    <a href="<?php echo BASE_URL; ?>/integrations/dashboard.php" class="btn btn-secondary">
                         <i class="fas fa-tachometer-alt"></i>
                         Dashboard
                     </a>
